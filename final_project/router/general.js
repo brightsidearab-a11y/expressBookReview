1181018db212e9333cbe1cfa2de904c074f56d27
brightsidearab-a11y <brightsidearab@gmail.com>
2026-02-10 11:02:12 +0100

    general.js

---
+++ final_project/router/general.js
@@ -0,0 +1,48 @@
+const express = require('express');
+let books = require("./booksdb.js");
+const public_users = express.Router();
+const axios = require('axios');
+
+// Task 10: Get all books using Promise/Async-Await
+public_users.get('/', async function (req, res) {
+  try {
+    const getBooks = () => new Promise((resolve) => resolve(books));
+    const allBooks = await getBooks();
+    return res.status(200).send(JSON.stringify(allBooks, null, 4));
+  } catch (err) {
+    res.status(500).json({message: "Error retrieving books"});
+  }
+});
+
+// Task 11: Get book details based on ISBN using Promises
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  new Promise((resolve, reject) => {
+    if (books[isbn]) resolve(books[isbn]);
+    else reject("Book not found");
+  })
+  .then(book => res.status(200).json(book))
+  .catch(err => res.status(404).json({message: err}));
+});
+  
+// Task 12: Get book details based on author using Promises
+public_users.get('/author/:author', function (req, res) {
+  const author = req.params.author;
+  new Promise((resolve) => {
+    let filteredBooks = Object.values(books).filter(b => b.author === author);
+    resolve(filteredBooks);
+  })
+  .then(result => res.status(200).json(result));
+});
+
+// Task 13: Get all books based on title using Promises
+public_users.get('/title/:title', function (req, res) {
+  const title = req.params.title;
+  new Promise((resolve) => {
+    let filteredBooks = Object.values(books).filter(b => b.title === title);
+    resolve(filteredBooks);
+  })
+  .then(result => res.status(200).json(result));
+});
+
+module.exports.general = public_users;
